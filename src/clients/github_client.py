c2c2cac2e4a006958bde236ebdf3de16caa25a21
Vince Silvey <vents54@outlook.com>
2024-11-29 09:52:20 -0600

    Add print statement to troubleshoot token

--- src/clients/github_client.py
+++ src/clients/github_client.py
@@ -25,7 +25,7 @@ class GithubClient:
             token (str): The GitHub token for authentication.
         """
         try:
-            print(f"the github token is: {token}")
+            logging.info("Github token is: %s", token)
             self.client = Github(token)
             self.repo_name = os.getenv('GITHUB_REPOSITORY')
             self.repo = self.client.get_repo(self.repo_name)
