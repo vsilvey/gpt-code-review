1d4124284c5a0715809bfdc590544462eea7b495
Vince Silvey <vents54@outlook.com>
2024-12-09 16:38:49 -0600

    Some log message and doc cleanup

--- src/main.py
+++ src/main.py
@@ -29,7 +29,6 @@ def main():
     reviewer = env_vars.get('GITHUB_REVIEWER', '').strip()
 
 # Proceed with code review if reviewer matches most recently requested reviewer.
-    owner = github_client.get_pr_owner(pr_id)
 
     if  github_client.get_most_recent_reviewer(owner, pr_id) == reviewer:
         logging.info("Reviewer %s assigned. Starting code review.", reviewer)
